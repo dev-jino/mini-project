41ece1f5c05bffb28b3e79436a1e20dedd981cef
dev-jino <wlsgh2029@naver.com>
2023-10-16 16:19:02 +0900

    feat : kakao login & daum address 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ import logo from "../assets/images/logo-img.png";
 import search_logo from "../assets/images/search-icon.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import HeaderMenu from "../components/HeaderMenu";
+
 function Header() {
   const [text, setText] = useState("");
   const onChange = (e) => {
@@ -14,21 +16,23 @@ function Header() {
       <div className={styles.container}>
         <div className={styles.div}>
           <div className={styles.logo_icon}>
-            <div className={styles.logo_text}><Link to='/'>HowMuch</Link></div>
-            
+          <Link to='/'>
+            <div className={styles.logo_text}>HowMuch</div>
             <img className={styles.logo_img} src={logo} />
+            </Link>
           </div>
           <div className={styles.header_input}><img className={styles.img} src={search_logo} />
           <form action='/search-result'>
               <input className={styles.search_inputbox} name="search-item" type="text" onChange={onChange} value={text} />
             </form>
           </div>
-          <div className={styles.main_menu}>
+          {/* <div className={styles.main_menu}>
             <div className={styles.menu_item}><div className={styles.menu_item_txt}><Link to='/cart'>장바구니</Link></div></div>
             <div className={styles.menu_item}><div className={styles.menu_item_txt}><Link to='/order-check'>주문조회</Link></div></div>
             <div className={styles.menu_item}><div className={styles.text_wrapper}><Link to='/mypage'>마이페이지</Link></div></div>
             <div className={styles.menu_item}><div className={styles.menu_item_txt}><Link to='/logout'>로그아웃</Link></div></div>
-          </div>
+          </div> */}
+          <HeaderMenu />
         </div>
       </div>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,6 @@
 import "./Login.css";
-import logo from "../assets/images/logo-img.png";
-import github_logo from "../assets/images/github-icon.png";
-import search_logo from "../assets/images/search-icon.png";
 import kakao_login_img from "../assets/images/kakao_login_img.png";
-import kakao_join_img from "../assets/images/kakao_join_img.png";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+
 function Login() {
   const Rest_api_key = `${process.env.REACT_APP_KAKAO_REST_API_KEY}`;
   const redirect_uri = `${process.env.REACT_APP_REDIRECT_URI}`;
@@ -13,48 +8,21 @@ function Login() {
 
   const MoveKakaoLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
-    // window.open(KAKAO_AUTH_URL, "KakaoLogin", "width=400, height=300");
   }
-
-  {/* ************************************ */}
-  const MoveKakaoLogout = () => {
-
-  }
-  {/* ************************************ */}
 
   return (
     <div class="login">
       <div class="wrapper">
-        {/* <Header /> */}
         <div class="main">
           <div class="container-2">
             <div class="title"><div class="title-msg">로그인</div></div>
             <div class="order-check-pre">
-              {/* <div class="button-confirm">
-                <div class="button-confirm-text">카카오 로그인</div>
-              </div> */}
               <button class="button-confirm" onClick={MoveKakaoLogin}>
                 <img class="" src={kakao_login_img}></img>
               </button>
             </div>
-            {/* ************************************ */}
-            <div class="title"><div class="title-msg">로그아웃</div></div>
-            <div class="order-check-pre">
-              <button class="button-confirm" onClick={MoveKakaoLogout}>
-                <img class="" src={kakao_login_img}></img>
-              </button>
-            </div>
-            {/* ************************************ */}
-            <div class="title"><div class="title-msg">회원이 아니시라면...?</div></div>
-            {/* <div class="button-confirm">
-              <div class="button-confirm-text">카카오로 시작하기</div>
-            </div> */}
-            <button class="button-confirm">
-              <img class="" src={kakao_join_img}></img>
-            </button>
           </div>
         </div>
-        {/* <Footer /> */}
       </div>
     </div>
   )
